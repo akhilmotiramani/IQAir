7832d305816187f10ccbe3c4b8ad067399844306
Akhil Motiramani <akhil.motiramani@yahoo.com>
2023-09-14 22:55:32 -0400

    Initial Commit

---
+++ IQAir/IQAirApp.swift
@@ -0,0 +1,17 @@
+//
+//  IQAirApp.swift
+//  IQAir
+//
+//  Created by Akhil Motiramani on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct IQAirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
